7d74f4fb1fdfb093fdf0adc80812e485343975a2
Dieter Mourisse <dieter.mourisse@howest.be>
2020-09-17 10:28:56 +0200

    Update to .NETCore App

--- Prb.Debugging.Wpf/App.xaml.cs
+++ Prb.Debugging.Wpf/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Bewerkingen.Wpf
+namespace Prb.Debugging.Wpf
 {
     /// <summary>
     /// Interaction logic for App.xaml
